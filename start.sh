6ea070ae86a231b9f2e02cc313a38e801207ed75
Bernardoc14 <147110226+Bernardoc14@users.noreply.github.com>
2026-06-21 22:51:12 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-echo "=== INFO BANCO ==="
-ls -lh banco.db
+if [ ! -f "banco.db" ]; then
+    echo "Baixando banco.db..."
 
-echo "=== PRIMEIRAS LINHAS ==="
-head -n 5 banco.db
+    wget --no-check-certificate \
+      "https://drive.google.com/uc?export=download&id=1Gh32rQd6YadaGRqwFUucWAVkD84EkXtD" \
+      -O banco.db
+fi
+
+echo "Tamanho do banco:"
+ls -lh banco.db
 
 streamlit run app.py --server.port $PORT --server.address 0.0.0.0
