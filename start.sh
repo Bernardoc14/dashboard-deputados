86b2553f5193a8d928f54a36d311ba1644ce64bd
Bernardoc14 <147110226+Bernardoc14@users.noreply.github.com>
2026-06-21 22:53:17 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 if [ ! -f "banco.db" ]; then
     echo "Baixando banco.db..."
 
-    wget --no-check-certificate \
-      "https://drive.google.com/uc?export=download&id=1Gh32rQd6YadaGRqwFUucWAVkD84EkXtD" \
-      -O banco.db
+    curl -L \
+    "https://drive.google.com/uc?export=download&id=1Gh32rQd6YadaGRqwFUucWAVkD84EkXtD" \
+    -o banco.db
 fi
 
 echo "Tamanho do banco:"
