35c69e8c7c4717bc49269a6fdd302e5a44aaa6bd
Bernardoc14 <147110226+Bernardoc14@users.noreply.github.com>
2026-06-21 22:43:13 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-if [ ! -f "banco.db" ]; then
-    echo "banco.db não encontrado. Rodando ETL..."
-    python etl_sqlite.py
-    echo "ETL concluído."
-else
-    echo "banco.db já existe. Pulando ETL."
-fi
+echo "=== INFO BANCO ==="
+ls -lh banco.db
+
+echo "=== PRIMEIRAS LINHAS ==="
+head -n 5 banco.db
 
 streamlit run app.py --server.port $PORT --server.address 0.0.0.0
